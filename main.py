fca318d925903fc9873dc0b4da7862c4a1607319
GianaTK7 <tkgenia123@gmail.com>
2025-09-11 22:54:27 +0200

    Resolved rout

--- main.py
+++ main.py
@@ -391,7 +391,7 @@ async def close_event(event_id: str = Path(...), attendees: list = None, did_not
 
 
 # Pseudo Python example
-@app.route('/events')
+@app.post('/events')
 def get_events():
     status = request.args.get('status')
     if status == 'open':
