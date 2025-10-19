64e58d0a40ac6586b19d750d08e3f7255bca2374
sir mufasa <jonathankabango08@gmail.com>
2025-10-19 22:44:40 +0200

    people-list

--- database.py
+++ database.py
@@ -1,26 +1,15 @@
-import os
-from motor.motor_asyncio import AsyncIOMotorClient
-from dotenv import load_dotenv
+import os 
+from motor.motor_asyncio import AsyncIOMotorClient 
+from dotenv import load_dotenv 
 
-load_dotenv()
+load_dotenv() 
 
 MONGO_URI = "mongodb+srv://activeteams:helloactiveteams@active-teams.ykghvqr.mongodb.net/"
-MAIN_DB_NAME = "active-teams-db"
-PEOPLE_DB_NAME = "testing-data-active-teams"  # 👈 separate database for People
-
+DB_NAME = "active-teams-db"
 client = AsyncIOMotorClient(MONGO_URI)
-
-# Main database (for most collections)
-db = client[MAIN_DB_NAME]
-
-# Separate database for People
-people_db = client[PEOPLE_DB_NAME]
-
-# Collections
+db = client[DB_NAME]
 events_collection = db["Events"]
+people_collection = db["People"]
 users_collection = db["Users"]
 tasks_collection = db["tasks"]
 # cells_collection = db["Cells"]
-
-# 👇 Only this one fetches from the testing database
-people_collection = people_db["people"]
