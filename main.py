620287c85cd32988f2dcbcf02aa9091738e838c7
Blessing <blessingjonathan028@gmail.com>
2026-04-09 13:31:19 +0200

    Removed the preflight response

--- main.py
+++ main.py
@@ -50,10 +50,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.options("/{rest_of_path:path}")
-async def preflight_handler(rest_of_path: str):
-    return JSONResponse(content={"message": "OK"})
-
 ORG_ID_MAP = {
     "active-church": "active-teams",
     "active church": "active-teams",
